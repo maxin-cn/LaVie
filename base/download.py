744f0138a775ac321dd4cedbf3fb4758ea255c6c
Xin Ma <38418898+maxin-cn@users.noreply.github.com>
2023-11-21 09:56:35 +1100

    Update download.py

--- base/download.py
+++ base/download.py
@@ -1,16 +1,7 @@
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
-import torch
 import os
-
+import torch
 
 def find_model(model_name):
-    """
-    Finds a pre-trained model, downloading it if necessary. Alternatively, loads a model from a local path.
-    """
     checkpoint = torch.load(model_name, map_location=lambda storage, loc: storage)
     if "ema" in checkpoint:  # supports checkpoints from train.py
         print('Ema existing!')
